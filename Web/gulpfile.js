97ba0101fd4abd2bfde98de70ce32fef2a6c3030
Peter <apprime@hotmail.com>
2017-11-04 19:56:42 +0100

    Added typescript support for dev and prod deploy. Moved less styles outside of wwwroot.

--- Web/gulpfile.js
+++ Web/gulpfile.js
@@ -2,27 +2,65 @@
 var gulp = require("gulp");
 var fs = require("fs");
 var less = require("gulp-less");
+var del = require('del');
 var concat = require("gulp-concat");
 var cssmin = require("gulp-cssmin");
 var cached = require("gulp-cached");
 
-gulp.task("convertLess:css", function () {
-    return gulp.src(['./wwwroot/less/**/*.less', './wwwroot/less/*.less'])
+var paths =
+{
+    scripts: ["../Client/scripts/**/*.js", "../Client/scripts/**/*.ts", "../Client/scripts/**/*.map"],
+    styles: ["Client/style/**/*.less", "Client/style/*.less"]
+};
+
+gulp.task('clean:dev', ['cleanDev:css', 'cleanDev:script']);
+
+gulp.task('clean:prod', ['cleanProd:css', 'cleanProd:script']);
+
+gulp.task('cleanDev:css', function ()
+{
+    return del(['wwwroot/css/**/*', '!wwwroot/css/site.css']);
+});
+
+gulp.task('cleanProd:css', function ()
+{
+    return del(['wwwroot/css/site.css']);
+});
+
+gulp.task('cleanDev:script', function ()
+{
+    return del(['wwwroot/scripts/**/*', '!wwwroot/scripts/site.js']);
+});
+
+gulp.task('cleanProd:script', function ()
+{
+    return del(['wwwroot/scripts/site.js']);
+});
+
+gulp.task('convert:typescript', function ()
+{
+    gulp.src(paths.scripts).pipe(gulp.dest('wwwroot/js'));
+});
+
+gulp.task("convertLess:css", function ()
+{
+    return gulp.src(paths.styles)
                .pipe(cached('css'))
                .pipe(less())
                .pipe(gulp.dest('wwwroot/css'));
 });
 
-gulp.task("compress:css", function () {
+gulp.task("compress:css", function ()
+{
     return gulp.src(['./wwwroot/css/**/*.css', './wwwroot/css/*.css'])
-               .pipe(concat('wwwroot/css/output/site.css'))
+               .pipe(concat('wwwroot/css/site.css'))
                .pipe(cssmin())
                .pipe(gulp.dest('.'));
 });
 
 gulp.task('watch', function () {
-    gulp.watch(['./wwwroot/less/**/*.less', './wwwroot/less/*.less'], ['dev']);
+    gulp.watch([paths.styles], ['dev']);
 });
 
-gulp.task("prod", ["convertLess:css", "compress:css"]);
-gulp.task("dev", ["convertLess:css"]);
+gulp.task("prod", ["clean:prod", "convertLess:css", "compress:css", "convert:typescript", "clean:dev" ]);
+gulp.task("dev", ["clean:dev", "convertLess:css", "convert:typescript" ]);
